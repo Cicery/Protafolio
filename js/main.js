3732623b75d016a55a701ba5a0cdfe4c33e256c9
Yhefferson <ycicery0@misena.edu.co>
2021-03-23 23:22:39 -0500

    Agregar proyecto e mi portafolio

---
+++ js/main.js
@@ -0,0 +1,70 @@
+let nav = document.getElementById('nav');
+let menu = document.getElementById('enlaces');
+let abrir = document.getElementById('open');
+let botones = document.getElementsByClassName('btn-header');
+let cerrado = true;
+
+function menus(){
+    let Desplazamiento_Actual = window.pageYOffset;
+    if (Desplazamiento_Actual <= 300){
+        nav.classList.remove('nav2');
+        nav.className = ('nav1');
+        nav.style.transition = '1s';
+        menu.style.top = '80px';
+        abrir.style.color = '#fff';
+    }else{
+        nav.classList.remove('nav1');
+        nav.className = ('nav2');
+        nav.style.transition = '1s';
+        menu.style.top = '100px';
+        abrir.style.color = '#000';
+    }
+}
+
+function apertura(){
+    if(cerrado){
+        menu.style.width = '70vw';
+        cerrado = false;
+    }else{
+        menu.style.width = '0%';
+        menu.style.overflow = 'hiden';
+        cerrado = true;
+    }
+}
+window.addEventListener('load',function(){
+    let onload = document.getElementById('onload');
+    let body = document.getElementById('body');
+    onload.remove();
+    console.log(body);
+    body.classList.remove('hidden');
+    nav.className = ('');
+    console.log(body);
+    menus();
+});
+
+window.addEventListener('scroll',function(){
+    console.log(window.pageYOffset);
+    menus();
+});
+window.addEventListener('resize',function(){
+        if(screen.width >= 700){
+            cerrado = true;
+            menu.style.removeProperty('overflow');
+            menu.style.removeProperty('width');
+
+        }
+});
+abrir.addEventListener('click',function(){        
+    apertura();
+});
+
+window.addEventListener('click',function(e){
+if(cerrado === false){
+let span = document.querySelector('span');
+if(e.target !== span && e.target !== abrir){
+    menu.style.width = '0%';
+    menu.style.overflow = 'hiden';
+    cerrado = true;
+}
+}   
+});
